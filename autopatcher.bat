1c06d7e06e6338f815103432bd480baff284acf7
Lojemiru <oneteenundergod@gmail.com>
2020-08-03 22:33:49 -0500

    Update autopatcher.bat

--- autopatcher.bat
+++ autopatcher.bat
@@ -169,15 +169,3 @@ REM ECHO The operation was completed successfully. See you next mission!
 del output.txt
 
 exit /b
-
-
-REM Pop-up prompt code below.
-:YesNoBox
-REM returns 6 = Yes, 7 = No. Type=4 = Yes/No
-set YesNo=
-set MsgType=4
-set heading=%~2
-set message=%~1
-echo wscript.echo msgbox(WScript.Arguments(0),%MsgType%,WScript.Arguments(1)) >"%temp%\input.vbs"
-for /f "tokens=* delims=" %%a in ('cscript //nologo "%temp%\input.vbs" "%message%" "%heading%"') do set YesNo=%%a
-exit /b
